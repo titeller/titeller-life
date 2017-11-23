506085ee4a1cf153f9023655f0d0242338fad73a
kitti piyapan <kitti.piyapan@gmail.com>
2017-11-23 12:47:27 +0700

    add responsive

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
               <iframe className="youtube-player" type="text/html" src={`https://www.youtube.com/embed/${video}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=${video}`} frameBorder="0" allowFullScreen title="xDeep Music" />
             </div>
           </div>
+          <div className="image-background" />
           <div className="text-center">
             <div className="wrapper">
               <img src="/me.jpg" className="me-avatar" alt="me-avatar" />
